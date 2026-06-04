19c2a9ce11ca5e2255fdc672e2d039fe29d85353
TacoMilitary <wlolams@outlook.com>
2026-06-04 18:10:02 -0500

    (Practice Problems) Problem 6

---
+++ lesson_2/problem6.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |name, info|
+  puts "#{name} is a #{info['age']}-year-old #{info['gender']}"
+end
